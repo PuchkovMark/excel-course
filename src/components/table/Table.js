f91afd4fc57636958df178b153fe3a92744aa7de
PuchkovMark <55077810+PuchkovMark@users.noreply.github.com>
2020-12-19 18:06:54 +0300

    finish base framework functional (#5)

--- src/components/table/Table.js
+++ src/components/table/Table.js
@@ -1,59 +1,9 @@
 import {ExcelComponent} from '@core/ExcelComponent'
+import {createTable} from './table.template'
 
 export class Table extends ExcelComponent {
     static className = 'excel__table'
     toHTML() {
-        return `
-        <div class="row">
-                <div class="row_info"></div>
-
-                <div class="row_data">
-                    <div class="column">
-                        A
-                    </div>
-                    <div class="column">
-                        B
-                    </div>
-                    <div class="column">
-                        C
-                    </div>
-                </div>
-            </div>
-            <div class="row">
-                <div class="row_info">
-                    1
-                </div>
-
-                <div class="row_data">
-                    <div class="cell selected" contenteditable>
-                        A1
-                    </div>
-                    <div class="cell" contenteditable>
-                        B2
-                    </div>
-                    <div class="cell" contenteditable>
-                        C3
-                    </div>
-                </div>
-            </div>
-            <div class="row">
-                <div class="row_info">
-                    2
-                </div>
-
-                <div class="row_data">
-                    <div class="cell" contenteditable>
-                        A1
-                    </div>
-                    <div class="cell" contenteditable>
-                        B2
-                    </div>
-                    <div class="cell" contenteditable>
-                        C3
-                    </div>
-                </div>
-            </div>
-        </div>
-        `
+        return createTable(20)
     }
 }
