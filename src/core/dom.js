2a300a374e39d8d959301fc2b6472f0afca1bfb1
PuckovMark <mark.puchkov19@gmail.com>
2020-12-20 21:29:50 +0300

    create resize cells

--- src/core/dom.js
+++ src/core/dom.js
@@ -39,6 +39,22 @@ class Dom {
         }
         return this
     }
+
+    get data() {
+        return this.$el.dataset
+    }
+
+    findAll(selector) {
+        return this.$el.querySelectorAll(selector)
+    }
+
+    closest(selector) {
+        return $(this.$el.closest(selector))
+    }
+
+    getCords() {
+        return this.$el.getBoundingClientRect()
+    }
 }
 
 // event.target
